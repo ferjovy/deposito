22a8fdc536ec518e88f397efff87486d8c64fc2c
isard <isard@DESKTOP-8KBMOR3>
2025-03-13 11:36:52 +0100

    commit 2

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,28 +1,53 @@
 package cuentas;
+
+/**
+ * Clase que nos permite ver los constructores, estado cuenta, ingresar, retirar, setters y getters
+ * @author Roberto Hernandez
+ */
 public class CCuenta {
 
-
+	/**
+	 * ATRIBUTOS
+	 */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+    
+    /**
+     * CONSTRUCTOR VACIO
+     */
     public CCuenta()
     {
     }
-
+    /**
+     * CONSTRUCTOR COMPLETO
+     * @param nom nombre
+     * @param cue cuenta
+     * @param sal saldo
+     * @param tipo tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    
+    /**
+     * Estado de la cuenta
+     * @return Saldo de la cuenta
+     */
     public double estado()
     {
         return saldo;
     }
-
+    
+    /**
+     * Ingresa una cantidad en cuenta
+     * @param cantidad a ingresar
+     * @throws Exception si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +55,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+   	 * Retira una cantidad de la cuenta
+   	 * @param Cantidad a retirar
+   	 * @throws Exception si no hay saldo suficiente o es negativa la cantidad
+   	 */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +69,58 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
 
+    /**
+     * GETTERS Y SETTERS
+     * GET nombre
+     * @return get nombre
+   	 */
 	public String getNombre() {
 		return nombre;
 	}
-
+    /**
+     * SET NOMBRE
+     * @return  set nombre
+   	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+    /**
+     * GET CUENTA
+     * @return  get cuenta
+   	 */
 	public String getCuenta() {
 		return cuenta;
 	}
-
+    /**
+     * SET CUENTA
+     * @return  set cuenta
+   	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
-
+    /** GET SALDO
+    * @return  get saldo
+  	 */
 	public double getSaldo() {
 		return saldo;
 	}
-
+    /**
+     * SET SALDO
+     * @return  set saldo
+   	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-
+    /** GET TIPO INTERES
+    * @return  get TipoInterés
+  	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
-
+    /**
+     * SET TIPO INTERES
+     * @return  set TipoInterés
+   	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
